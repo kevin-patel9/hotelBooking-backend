1556c6440fbcb13783f35d924daea9d18693f733
Kevin Patel <kevinpatel0129@gmail.com>
2022-11-26 23:05:04 +0530

    cors adding

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const cookieParser = require("cookie-parser");
-const cors = require("cors");
+// const cors = require("cors");
 const hotels = require("./routes/hotel");
 const users = require("./routes/user");
 const rooms = require("./routes/room");
@@ -10,9 +10,17 @@ require("dotenv").config();
 const auth = require("./routes/auth");
 const path = require('path');
 
-app.use(cors({
-  origin: '*'
-}));
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, x-auth-token");
+    next();
+  });
+
+// app.use(cors({
+//   origin: '*'
+// }));
 app.use(cookieParser());
 
 mongoose
@@ -28,6 +36,7 @@ app.use("/users", users);
 app.use("/rooms", rooms);
 app.use(function (err, req, res, next) {
   res.status(500).send("Something Failed");
+  next();
 });
 
 app.use(express.static(path.join(__dirname, "/client/build")));
