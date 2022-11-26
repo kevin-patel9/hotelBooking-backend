bfcf92d5bf7728a0eb20cc876f84cee987f822f0
Kevin Patel <kevinpatel0129@gmail.com>
2022-11-26 23:38:49 +0530

    cors

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ const rooms = require("./routes/room");
 require("dotenv").config();
 const auth = require("./routes/auth");
 const path = require('path');
-const cors = require('cors');
+// const cors = require('cors');
 
 
-const corsOptions ={
-    origin:'https://booking-hotelss.herokuapp.com', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-app.use(cors(corsOptions));
+// app.use(cors());
 app.use(cookieParser());
 
 mongoose
