af8721af1b1bfee15301474fec3925c038d217bf
Kevin Patel <kevinpatel0129@gmail.com>
2022-11-26 23:43:37 +0530

    cors update

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://booking-hotelss.herokuapp.com/"
+    baseURL: "https://hotels-booking.herokuapp.com"
 })
