62388989f586bd029ae9d5c7cc150d9c7de042b2
Kevin Patel <kevinpatel0129@gmail.com>
2022-11-27 23:11:35 +0530

    server changes

--- index.js
+++ index.js
@@ -36,6 +36,12 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
 });
 
+app.use(express.static(path.join(__dirname, "/admin/build")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/admin/build', 'index.html'));
+});
+
 const port = process.env.PORT || 9000;
 
 app.listen(port, ()=> console.log(`Listining to port ${port}`))
